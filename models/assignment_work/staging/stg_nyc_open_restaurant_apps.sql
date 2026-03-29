bb9bb62700967532bcc6a9d0e55c070088ef71a6
kazi islam <jewel1619@gmail.com>
2026-03-29 21:03:50 +0000

    creating the sql file for staging second table nyc_open_restaurant_apps

---
+++ models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
@@ -0,0 +1,86 @@
+-- Clean and standardize NYC Open Restaurant Apps data
+-- One row per service request
+
+WITH source AS (
+   SELECT * FROM {{ source('raw', 'source_nyc_open_restaurant_apps') }}
+), -- Easier to refer to the dbt reference to a long name table this way
+
+cleaned AS (
+   SELECT
+       -- Get all columns from source, except ones we're transforming below
+       -- To do cleaning on them or explicitly cast them as types just in case
+       * EXCEPT (
+           objectid,
+           time_of_submission,
+           restaurant_name,
+           legal_business_name,
+           doing_business_as,
+           seating_interest,
+           postcode,
+           borough,
+           street,
+           building_number,
+           latitude,
+           longitude
+       ),
+
+       -- Identifiers
+       CAST(objectid AS STRING) AS application_id,
+
+       -- Date/Time
+       CAST(time_of_submission AS TIMESTAMP) AS created_date,
+
+       -- Request details
+       CAST(restaurant_name AS STRING) AS restaurant_name,
+       CAST(legal_business_name AS STRING) AS legal_business_name,
+       CAST(doing_business_as AS STRING) AS dba_name,
+       CAST(seating_interest AS STRING) AS seating_type,
+       UPPER(TRIM(CAST(status AS STRING))) AS status,
+
+       -- Location - clean zip code, handling several common zip code data problems
+       CASE
+           WHEN UPPER(TRIM(CAST(postcode AS STRING))) IN ('N/A', 'NA') THEN NULL
+           WHEN UPPER(TRIM(CAST(postcode AS STRING))) = 'ANONYMOUS' THEN 'Anonymous'
+           WHEN LENGTH(CAST(postcode AS STRING)) = 5 THEN CAST(postcode AS STRING)
+           WHEN LENGTH(CAST(postcode AS STRING)) = 9 THEN CAST(postcode AS STRING)
+           WHEN LENGTH(CAST(postcode AS STRING)) = 10
+               AND REGEXP_CONTAINS(CAST(postcode AS STRING), r'^\d{5}-\d{4}')
+           THEN CAST(postcode AS STRING)
+           ELSE NULL
+       END AS postcode,
+
+       -- Location - standardized borough, just in case
+       CASE
+           WHEN UPPER(TRIM(borough)) IN ('MANHATTAN', 'NEW YORK COUNTY') THEN 'Manhattan'
+           WHEN UPPER(TRIM(borough)) IN ('BRONX', 'THE BRONX') THEN 'Bronx'
+           WHEN UPPER(TRIM(borough)) IN ('BROOKLYN', 'KINGS COUNTY') THEN 'Brooklyn'
+           WHEN UPPER(TRIM(borough)) IN ('QUEENS', 'QUEEN', 'QUEENS COUNTY') THEN 'Queens'
+           WHEN UPPER(TRIM(borough)) IN ('STATEN ISLAND', 'RICHMOND COUNTY') THEN 'Staten Island'
+           ELSE 'UNKNOWN or CITYWIDE'
+       END AS borough,
+
+       CAST(street AS STRING) AS street_address,
+       CAST(building_number AS STRING) AS building_number,
+       CAST(latitude AS DECIMAL) AS latitude,
+       CAST(longitude AS DECIMAL) AS longitude,
+
+
+
+       -- Metadata
+       CURRENT_TIMESTAMP() AS _stg_loaded_at
+
+   FROM source
+
+   -- Filters
+   WHERE (agency = 'DOT' OR agency_name LIKE '%Transportation%')
+   AND objectid IS NOT NULL
+   AND time_of_submission IS NOT NULL
+   AND CAST(time_of_submission AS DATE) >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 YEAR)
+   AND borough IS NOT NULL
+
+   -- Deduplicate
+   QUALIFY ROW_NUMBER() OVER (PARTITION BY objectid ORDER BY created_date DESC) = 1
+)
+
+SELECT * FROM cleaned
+-- All should be part of this table: stg_nyc_open_restaurant_apps
