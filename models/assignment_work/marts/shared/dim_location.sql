232a494cb9b82a5fb5c92ebef9e3328b9aa5cae3
kazi islam <jewel1619@gmail.com>
2026-03-30 03:08:35 +0000

    updated the sql file

--- models/assignment_work/marts/shared/dim_location.sql
+++ models/assignment_work/marts/shared/dim_location.sql
@@ -2,7 +2,7 @@
 
 WITH all_locations AS (
    -- Get locations from 311 requests
-   SELECT DISTINCT CAST (borough, incident_zip AS zip_code)
+   SELECT DISTINCT borough, incident_zip AS zip_code
       --TODO replace (HINT: look @ dimensional model & staging data!)
    FROM {{ ref('stg_nyc_311_dot') }}
    WHERE borough IS NOT NULL
@@ -10,7 +10,7 @@ WITH all_locations AS (
    UNION DISTINCT
 
    -- Get locations from restaurant applications
-   SELECT DISTINCT CAST (borough, zip AS zip_code)
+   SELECT DISTINCT borough, zip AS zip_code
        -- TODO replace (HINT: look @ dimensional model & staging data!)
    FROM {{ ref('stg_nyc_open_restaurant_apps') }}
    WHERE borough IS NOT NULL
